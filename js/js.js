42ceb7164fbaa229c3ea63277679d0fe430cd1ce
Front Galgo <98433863+eLgalgo@users.noreply.github.com>
2022-06-25 00:21:56 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,54 @@
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var scrollToTopBtn2 = document.querySelector(".scrollToTopBtn2");
+var rootElement = document.documentElement;
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.08) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
+    scrollToTopBtn2.classList.add("showBtn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
+    scrollToTopBtn2.classList.remove("showBtn");
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
+
+//Secciones
+function galeria(){
+  
+}
+
+//FOTOS
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
